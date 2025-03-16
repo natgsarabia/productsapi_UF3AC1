6fde165c24f78ff3fea7c3b807647d884a7c138d
natgsarabia <natalia.garcia@estudiant.fjaverianas.com>
2025-03-16 21:30:49 +0100

    variable entorno mongouri

--- config/dist/config.dev.js
+++ config/dist/config.dev.js
@@ -1,14 +1,15 @@
+"use strict";
+
 require('dotenv').config();
 
-const mongoURI = process.env.MONGOURI;
+var mongoURI = process.env.MONGOURI;
 
 if (!mongoURI) {
   console.error("¡Error! La variable de entorno MONGOURI no está definida.");
   process.exit(1); // Termina el proceso si la URI no está definida
 }
 
-console.log("MongoURI:", mongoURI); 
-
+console.log("MongoURI:", mongoURI);
 module.exports = {
-  mongoURI
+  mongoURI: mongoURI
 };
